66e8b11e8d52e7e30b625a60c3a768b40b3e45f0
hazbiin <hzbi717@gmail.com>
2025-07-02 22:45:41 +0530

    test push

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
   import Navbar from '@/components/Navbar.vue';
+  import { RouterView } from 'vue-router';
 </script>
 
-
 <template>
-  <Navbar/>
+  <Navbar />
+  <RouterView />
 </template>
